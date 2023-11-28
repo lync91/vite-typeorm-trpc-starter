9b75afc11638784b6c4cc64d9e8ad381aebf06c7
lync91 <mr.lync91@gmail.com>
2023-11-28 17:23:54 +0700

    add code

---
+++ src/db/data-source.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { User } from './entities/User'
+
+const migrationPath = `${__dirname}/src/**/migrations/*.{ts,js}`
+console.log(migrationPath);
+
+
+const port = process.env.DB_PORT as number | undefined
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: port,
+  username: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  entities: [User],
+  migrations: [migrationPath],
+})
